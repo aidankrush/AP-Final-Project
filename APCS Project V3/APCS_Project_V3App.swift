ab7a59235765ebe3ba9e539a820c8867d0811f65
aidankrush <ajkrush23@icloud.com>
2023-04-17 08:40:19 -0500

    Initial Commit

---
+++ APCS Project V3/APCS_Project_V3App.swift
@@ -0,0 +1,17 @@
+//
+//  APCS_Project_V3App.swift
+//  APCS Project V3
+//
+//  Created by Aidan Krush23 on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct APCS_Project_V3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
